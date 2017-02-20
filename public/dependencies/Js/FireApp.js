0fcc29e875761a0d4a758b634b52a61b770aab17
José Luis Raffalli <raffallijoseluis@gmail.com>
2017-02-20 16:28:13 -0400

    Backend con js

--- public/dependencies/Js/FireApp.js
+++ public/dependencies/Js/FireApp.js
@@ -8,12 +8,27 @@
 	    messagingSenderId: "579979923419"
 	  };
 
+	  // Inicializa Firebase
 	  firebase.initializeApp(config);
 	  var db = firebase.database();
   	  var auth = firebase.auth();
 
-  	  //LOGIN Obtener los elementos
-	  var submitButton = document.getElementsByName("email");
-	  var emailInput = document.getElementsByName("password");
+  	  // Sesión form: Obtener los elementos
+  	  var btnLogin = document.getElementById("btnLogin");
+  	  var btnSignup = document.getElementById("btnSignup");
+	  var txtEmail = document.getElementById("email");
+	  var txtContraseña = document.getElementById("contraseña");
 
+	  // Evento de Login
+	  btnLogin.addEventListener("click" , function(){
+	  	// Valores de los campos
+	  	var email = txtEmail.value;
+	  	var contraseña = txtContraseña.value;
+	  	// Autentica
+	  	var promesa = auth.signInWithEmailAndPassword(email, contraseña);
+	  	promesa.catch(e => console.log(e.message));
+
+	  });
+
+	  // Evento de Signup
 }());
